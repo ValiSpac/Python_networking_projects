90c06cb7db20390fad4420a64254a3306538ff2d
SpacVali <spacvalentin1@gmail.com>
2023-12-05 12:08:22 +0200

    finished address, mask, host calc

--- SubnetCalc/Subnet_calculator.py
+++ SubnetCalc/Subnet_calculator.py
@@ -5,11 +5,11 @@ def subnet_calc():
 #Checking for ip address
     while True:
         ip_address = input("Enter and IP address: ")
-        octet_list = ip_address.split('.')
-        if (len(octet_list) == 4 and (1 <= int(octet_list[0]) <= 253) and
-            (int(octet_list[0]) != 127) and (int(octet_list[0]) != 169) and
-            int(octet_list[1]) != 254 and (0 <= int(octet_list[1]) <= 255)
-            and 0 <= int(octet_list[2]) <= 255 and 0 <= int(octet_list[3])
+        ip_octets = ip_address.split('.')
+        if (len(ip_octets) == 4 and (1 <= int(ip_octets[0]) <= 253) and
+            (int(ip_octets[0]) != 127) and (int(ip_octets[0]) != 169) and
+            int(ip_octets[1]) != 254 and (0 <= int(ip_octets[1]) <= 255)
+            and 0 <= int(ip_octets[2]) <= 255 and 0 <= int(ip_octets[3])
             <= 255):
             break
         else:
@@ -42,7 +42,6 @@ def subnet_calc():
     #Formula for getting the number of hosts/subnet
     #abs() because in the case of the /32 mask the no_of_zero = 0
     no_of_hosts = abs(2 ** no_of_zeros - 2)
-    print(f"host = {no_of_hosts}")
 
     #Get wildcard mask from octets
     wildcard_octets = []
@@ -51,10 +50,52 @@ def subnet_calc():
         wildcard_octets.append(str(wild_octet))
 
     wildcard_mask = '.'.join(wildcard_octets)
-    print(wildcard_mask)
+    #print(wildcard_mask)
 
+    #Extract each octet in the ip and put it into a string
     ip_octets_binary = []
     for octet in ip_octets:
         binary_octet = bin(int(octet)).lstrip('0b')
+        ip_octets_binary.append(binary_octet.zfill(8))
 
+    binary_ip = "".join(ip_octets_binary)
+    #print(binary_ip)
+
+    network_address_bin = binary_ip[:(no_of_ones )] + '0' * no_of_zeros
+    brodcasat_addres_bin = binary_ip[:(no_of_ones)] + '1' * no_of_zeros
+
+    #Get each octet from the network binary
+    net_ip_octets = []
+    for binn in range(0, 32, 8):
+        net_ip_octet = network_address_bin[binn : binn + 8]
+        net_ip_octets.append(net_ip_octet)
+
+    #Transform from binary to int the network address and join it to a string
+    net_ip_addresss = []
+    for each_octet in net_ip_octets:
+        net_ip_addresss.append(str(int(each_octet, 2)))
+
+    network_address = ".".join(net_ip_addresss)
+
+    #Get each octet from the brodcast address binary
+    bst_ip_octets = []
+    for binn in range(0, 32, 8):
+        bst_ip_octet = brodcasat_addres_bin[binn : binn + 8]
+        bst_ip_octets.append(bst_ip_octet)
+
+    #Transform from binary to int the brodcast address and join it to a string
+    bst_ip_address = []
+    for each_octet in bst_ip_octets:
+        bst_ip_address.append(str(int(each_octet, 2)))
+
+    broadcast_address = ".".join(bst_ip_address)
+    
+    print("\n")
+    print("Network address is: %s" % network_address)
+    print("Broadcast address is: %s" % broadcast_address)
+    print("Number of valid hosts per subnet: %s" % no_of_hosts)
+    print("Wildcard mask: %s" % wildcard_mask)
+    print("Mask bits: %s" % no_of_ones)
+    print("\n")
+    
 subnet_calc()
